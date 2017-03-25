a98bde324c12914e28bf6c6a3beccfdc57c2245f
María Celeste <Mara Celeste@MacheHP>
2017-03-25 10:48:40 -0400

    Fixed when there's a flag and it does not paint to gray

--- src/MyMouseAdapter.java
+++ src/MyMouseAdapter.java
@@ -77,19 +77,23 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Do nothing
 					} else {
 						//Released the mouse button on the same cell where it was pressed
-//						if ((gridX == 0) || (gridY == 0)) {
+						if ((gridX == 0) || (gridY == 0)) {
 							Color newColor = Color.GRAY;
 							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
 							myPanel.repaint();
-//						} else {
-//							//On the grid other than on the left column and on the top row:
-//							Color newColor = Color.GRAY;
-//							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-//							myPanel.repaint();
-//						}
+						} else if (myPanel.colorArray[gridX][gridY].equals(Color.RED)) {
+//							Do nothing
+						}
+						else{
+							//On the grid other than on the left column and on the top row:
+							Color newColor = Color.GRAY;
+							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+							myPanel.repaint();
+							}
+						}
 					}
 				}
-			}
+			
 			myPanel.repaint();
 			break;
 		case 3:		Component c1 = e.getComponent();
